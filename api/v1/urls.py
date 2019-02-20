e4f2ab9aa6f9314fc3b3d72ea980753ef54f94ff
Sergey Shishov <sshishov.sshishov@gmail.com>
2019-02-20 22:48:55 +0300

    Move `docs` under `/api/` endpoint

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include
 from django.urls import path
-
+from rest_framework.documentation import include_docs_urls
 from rest_framework.routers import DefaultRouter
 
 from . import views
@@ -15,6 +15,9 @@ router.register(prefix=r'gamesparks', viewset=gs_views.GameSparksViewSet, base_n
 
 
 urlpatterns = [
+    # Documentation
+    path('docs/', include_docs_urls(title='My API title', authentication_classes=[], permission_classes=[])),
+    # API endpoints
     path('user/login/', jwt_views.UserTokenObtainPairView.as_view(), name='user_token_obtain_pair'),
     path('anonymous/login/', jwt_views.AnonymousTokenObtainPairView.as_view(), name='anonymous_token_obtain_pair'),
     path('token/refresh/', jwt_views.CustomTokenRefreshView.as_view(), name='token_refresh'),
