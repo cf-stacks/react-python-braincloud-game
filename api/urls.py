6452447204e13381c85322d1993114337f1b2637
Sergey Shishov <sshishov.sshishov@gmail.com>
2019-02-20 22:50:40 +0300

    Fix the typo

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import include
 from django.urls import path
+from rest_framework.documentation import include_docs_urls
+
 
 urlpatterns = [
+    # Documentation
+    path('docs/', include_docs_urls(title='Sesam API', authentication_classes=[], permission_classes=[])),
+    # Version 1
     path('v1/', view=include(('api.v1.urls', 'api-v1'), namespace='v1')),
 ]
--- api/v1/urls.py
+++ api/v1/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import include
 from django.urls import path
-from rest_framework.documentation import include_docs_urls
 from rest_framework.routers import DefaultRouter
 
 from . import views
@@ -15,8 +14,6 @@ router.register(prefix=r'gamesparks', viewset=gs_views.GameSparksViewSet, base_n
 
 
 urlpatterns = [
-    # Documentation
-    path('docs/', include_docs_urls(title='My API title', authentication_classes=[], permission_classes=[])),
     # API endpoints
     path('user/login/', jwt_views.UserTokenObtainPairView.as_view(), name='user_token_obtain_pair'),
     path('anonymous/login/', jwt_views.AnonymousTokenObtainPairView.as_view(), name='anonymous_token_obtain_pair'),
