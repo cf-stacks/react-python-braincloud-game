334fcaebb9c362af2259c60b01c033806e03aaad
Sergey Shishov <sshishov.sshishov@gmail.com>
2019-03-24 15:35:03 +0300

    Move debug_toolbar to development

--- settings/base.py
+++ settings/base.py
@@ -57,7 +57,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'debug_toolbar',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
 
--- settings/development.py
+++ settings/development.py
@@ -3,3 +3,5 @@ import os
 os.environ['S_DEBUG'] = os.environ.get('S_DEBUG', 'True')
 
 from .base import *  # noqa: F403  # pylint:disable=wildcard-import,wrong-import-position
+
+INSTALLED_APPS = INSTALLED_APPS + ('debug_toolbar',)
