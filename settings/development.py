9a6060887ed2a10cabfe2282ec092c478d8a3b62
Sergey Shishov <sshishov.sshishov@gmail.com>
2019-03-24 15:37:40 +0300

    Additional move of tolbar devug

--- settings/development.py
+++ settings/development.py
@@ -4,4 +4,5 @@ os.environ['S_DEBUG'] = os.environ.get('S_DEBUG', 'True')
 
 from .base import *  # noqa: F403  # pylint:disable=wildcard-import,wrong-import-position
 
+MIDDLEWARE = ['debug_toolbar.middleware.DebugToolbarMiddleware'] + MIDDLEWARE
 INSTALLED_APPS = INSTALLED_APPS + ('debug_toolbar',)
