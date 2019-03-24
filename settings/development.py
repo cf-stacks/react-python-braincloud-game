b36477b686a9c12a6da45e6bf2c23d9934028e9f
Sergey Shishov <sshishov.sshishov@gmail.com>
2019-03-24 15:45:16 +0300

    Fix development settings INSTALLED_APPS

--- settings/development.py
+++ settings/development.py
@@ -7,4 +7,4 @@ ALLOWED_HOSTS = []
 from .base import *  # noqa: F403  # pylint:disable=wildcard-import,wrong-import-position
 
 MIDDLEWARE = ['debug_toolbar.middleware.DebugToolbarMiddleware'] + MIDDLEWARE
-INSTALLED_APPS = INSTALLED_APPS + ('debug_toolbar',)
+INSTALLED_APPS = INSTALLED_APPS + ['debug_toolbar',]
