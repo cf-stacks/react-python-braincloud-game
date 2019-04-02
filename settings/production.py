cb01dde0f7678bf8e17622d364ac2f461912364e
Sergey Shishov <sshishov.sshishov@gmail.com>
2019-04-02 22:23:20 +0300

    Finishing production setup

--- settings/production.py
+++ settings/production.py
@@ -2,6 +2,6 @@ import os
 
 os.environ['S_DEBUG'] = os.environ.get('S_DEBUG', '0')
 
-ALLOWED_HOSTS = ['52.193.213.171', 'localhost']
+ALLOWED_HOSTS = ['52.193.213.171']
 
 from .base import *  # noqa: F403  # pylint:disable=wildcard-import,wrong-import-position
